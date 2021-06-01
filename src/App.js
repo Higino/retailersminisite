ba2351852575964ffdd8a67fb2ef70407b5db41e
Higino Silva <higino@gmail.com>
2021-06-01 10:07:37 +0100

    Implement cart and cart badge in nav

--- src/App.js
+++ src/App.js
@@ -9,25 +9,34 @@ const App = () => {
   /// Static information
   const [prod1] = useState({id: 1, name: 'Shoes', description: 'These are <b>running shoes<b>', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.701xh;0,0.229xh&resize=1200:*' })
   const [prod2] = useState({id: 2, name: 'MacBook', description: 'Apple macbook. A great <b>computer</b>', price: '$1200', image: 'https://www.bhphotovideo.com/images/images2500x2500/apple_z0wq_mv96_12_bh_13_3_macbook_pro_with_1481053.jpg' })
+  const [prod3] = useState({id: 3, name: 'MacBookAir', description: 'Apple air book. A great <b>laptop</b>', price: '$750', image: 'https://www.bhphotovideo.com/images/images2500x2500/apple_z0wq_mv96_12_bh_13_3_macbook_pro_with_1481053.jpg' })
+  const [prod4] = useState({id: 4, name: 'Runner Shoes', description: 'These are <b>great running shoes<b>', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.701xh;0,0.229xh&resize=1200:*' })
 
   const [products, setProducts] = useState([])
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState({line_items: []})
+  const [cartSize, setCartSize] = useState(0)
   
+  const handleAddToCart = async (product, quantity) => {
+    const newCart = cart
+    newCart.line_items.push(product)
+    setCart( newCart )
+    setCartSize( newCart.line_items.length)
+  }
+
   const fetchCart = async () => {
     // Use Strapi CMS to get the user cart state, but for now this is static
     // const response = await strapi.cart.get()
-    const cart = {lineItems: [prod1], shippingInfo: {}, discountCodes: [], currency: 'Eur', }
+    const cart = {line_items: [] }
     setCart(cart)
+    setCartSize(cart.line_items.length)
   }
 
-  const handleAddToCart = async (produtId, quantity) => {
-    console.log("This would add something to your cart ("+produtId+", "+quantity+")")
-  }
+ 
 
   const fetchProducts = async () => {
     // Use Strapi CMS to get the list of products, but for now this is static
     // const response = await strapi.products.list()
-    const products = [prod1, prod2]
+    const products = [prod1, prod2, prod3, prod4]
     setProducts(products)
   }
 
@@ -37,9 +46,10 @@ const App = () => {
   }, [])
 
 
+  console.log(cart)
   return (
     <div>
-      <Navbar />
+      <Navbar cartSize={cart.line_items.length}/>
       <Products products={products} onAddToCart={handleAddToCart}/>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,8 +4,9 @@ import { ShoppingCart } from '@material-ui/icons'
 import logo from '../../assets/creoate-cropped-final-logo-black-2.png'
 import useStyles from './styles'
 
-const Navbar = () => {
+const Navbar = ({cartSize}) => {
     const classes = useStyles()
+    console.log('Cart size = ' + cartSize)
     return (
         <>
             <AppBar position='fixed' className={classes.appBar} color='inherit'>
@@ -16,7 +17,7 @@ const Navbar = () => {
                     <div className={classes.grow} />
                     <div className={classes.button}>
                         <IconButton aria-label='Show cart items' color='inherit'>
-                            <Badge badgeContent={2} color='secondary'>
+                            <Badge badgeContent={cartSize} color='secondary'>
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
